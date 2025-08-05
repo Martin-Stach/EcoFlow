fc5704487836e81882f78ff2fe3b6529d7229754
MortnMX <mortnmx@hotmail.com>
2025-08-05 10:32:33 +0200

    Remove sensitive credentials and implement secure config system

---
+++ powerprio-config.template.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This is a template file. Copy this to powerprio-config.sh and add your real credentials there.
+# powerprio-config.sh is gitignored and won't be committed.
+
+ACCESS_KEY="YOUR_ACCESS_KEY_HERE"
+DEVICE_SERIAL="YOUR_DEVICE_SERIAL_HERE"
